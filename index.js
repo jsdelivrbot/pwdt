e6b91cb904b9b85ee28f4f59d41b3216b81f08f3
Huang <Lilith.Huang@cshs.org>
2018-03-07 13:37:14 -0800

    error messages

--- index.js
+++ index.js
@@ -10,15 +10,23 @@ var app = express();
 app.use(express.static(path.join(__dirname, '/public')))
 app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('pages/index')) //route
+app.get('/', function (req, res) {
+  console.log("hello what's up")
+  res.render('pages/index')
+}); //route
 
 app.get('/db', function (request, response) {
+  console.log(process.env.DATABASE_URL);
+  console.log("hello again");
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  	if (err) {
+  		console.log("encountered error");
+  	}
     client.query('SELECT * FROM test_table;', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
-      else
+      elseherok
        { response.render('pages/db', {results: result.rows} ); }
     });
   });
