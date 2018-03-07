f7bd803949e481a37e92123e235933b9cb1e57e3
Huang <Lilith.Huang@cshs.org>
2018-03-06 16:39:18 -0800

    setting up database

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
+var pg = require('pg');
+
+// set port
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+app.use(express.static(path.join(__dirname, '/public')))
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index')) //route
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`)) 
